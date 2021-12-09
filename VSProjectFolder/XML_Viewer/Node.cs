55c0a4c4b52bb422c6bfb7e7914a4ac0c236ff5f
SallyHesham <sallyhesham@live.com>
2021-12-09 17:57:14 +0200

    added node and tree classes
    these classes are not final, they are just blueprints.

---
+++ VSProjectFolder/XML_Viewer/Node.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML_Viewer
+{
+    class Node
+    {
+        private string tag;
+        private string data;
+        private List<Node> children = new List<Node>();
+
+        Node(string t, string d)
+        {
+            tag = t;
+            data = d;
+        }
+
+        public void addChild(string t, string d)
+        {
+            Node child = new Node(t, d);
+            children.Add(child);
+            return;
+        }
+
+        public string getTag()
+        {
+            return tag;
+        }
+
+        public string getData()
+        {
+            return data;
+        }
+
+        public List<Node> getChildren()
+        {
+            return children;
+        }
+    }
+}
---
+++ VSProjectFolder/XML_Viewer/Tree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML_Viewer
+{
+    class Tree
+    {
+        private Node root;
+
+        Tree()
+        {
+            root = null;
+        }
+
+        public void addNode(string t, string d)
+        {
+
+        }
+
+    }
+}
